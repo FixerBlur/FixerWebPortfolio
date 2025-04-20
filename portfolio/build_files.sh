23698788a270b6992dd92ed078cf3183f3433fd4
Fixer Blur <fixerblur@proton.me>
2025-04-20 21:43:47 +0300

    update pillow

--- portfolio/build_files.sh
+++ portfolio/build_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+python manage.py collectstatic --noinput
+apt-get update && apt-get install -y libjpeg-dev zlib1g-dev 
